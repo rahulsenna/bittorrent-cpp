f7f83d16e25975bd7bbccae85a57b097d95cdfc2
Rahul Senna <rahulvsenna@gmail.com>
2025-08-23 09:06:12 +0530

    debug true

--- your_program.sh
+++ your_program.sh
@@ -12,14 +12,15 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
-)
+# (
+  # cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+  # cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  # cmake --build ./build
+# )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec $(dirname $0)/build/bittorrent "$@"
+# exec $(dirname $0)/build/bittorrent "$@"
+exec /Users/agentofchaos/dev/codecrafters-projects/bittorrent-cpp/build/bittorrent "$@"
